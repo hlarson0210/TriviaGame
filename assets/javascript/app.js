f93bcbfc6d0c8c4a67ec861225d1e84991f37c30
Hayden Larson <hlarson398@gmail.com>
2019-07-29 08:36:13 -0500

    added click events and functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,37 +7,99 @@ var unansweredQs = [];
 
 // var for question array with object:
 var questions = [
-    {1: "Question 1: How many modes does the game have?",
-    //options include: 7, 2, 1, 4
-    answer: "2"},
-    {2: "Question 2: When was the game created?",
-    //options include: March 1, 2016; June 30, 2017; July 25, 2017; July 20, 2016;
-    answer: "July 25, 2017"},
-    {3: "Question 3: Who developed the game?",
-    //options include: Epic Games, Nintendo, EA Sports, Disney
-    answer: "Epic Games"},
-    {4: "Question 4: How many players participate in Fortnite battle royale?",
-    //options include: 10, 100, 50, however many you want
-    answer: "100"},
-    {5: "Question 5: In-game purchase in Fornite battle royale are called?",
-    //options include: Purchases, Battle Pass, Royale Pass, Battle Royale Pass
-    answer: "Battle Pass"},
-    {6: "Question 6: What is the maximum health you can have?",
-    //options include: 50, 100, 150, 200
-    answer: "100"},
-    {7: "Question 7: What is the first weapon you are given in battle royale?",
-    //options include: Pistol, Glider, Pick Axe, None
-    answer: "Pick Axe"},
-    {8: "Question 8: The name of the hot air balloon is?",
-    //options include: Battle Bus, Battle Balloon, Battle Shuttle, Battle Ship
-    answer: "Battle Bus"},
-    {9: "Question 9: The shrinking map in battle royale is called?",
-    //options include: Poison, Storm, Hurricane, Death
-    answer: "Storm"},
-    {10: "Question 10: Which one of these is not a level of weapon?",
-    //options include: Rare, Epic, Normal, Legendary
-    answer: "Normal"}
+    {
+        question1: "Question 1: How many modes does the game have?",
+        //options include: 7, 2, 1, 4
+        answer1: "2"
+    },
+    {
+        2: "Question 2: When was the game created?",
+        //options include: March 1, 2016; June 30, 2017; July 25, 2017; July 20, 2016;
+        answer2: "July 25, 2017"
+    },
+    {
+        3: "Question 3: Who developed the game?",
+        //options include: Epic Games, Nintendo, EA Sports, Disney
+        answer3: "Epic Games"
+    },
+    {
+        4: "Question 4: How many players participate in Fortnite battle royale?",
+        //options include: 10, 100, 50, however many you want
+        answer4: "100"
+    },
+    {
+        5: "Question 5: In-game purchase in Fornite battle royale are called?",
+        //options include: Purchases, Battle Pass, Royale Pass, Battle Royale Pass
+        answer5: "Battle Pass"
+    },
+    {
+        6: "Question 6: What is the maximum health you can have?",
+        //options include: 50, 100, 150, 200
+        answer6: "100"
+    },
+    {
+        7: "Question 7: What is the first weapon you are given in battle royale?",
+        //options include: Pistol, Glider, Pick Axe, None
+        answer7: "Pick Axe"
+    },
+    {
+        8: "Question 8: The name of the hot air balloon is?",
+        //options include: Battle Bus, Battle Balloon, Battle Shuttle, Battle Ship
+        answer8: "Battle Bus"
+    },
+    {
+        9: "Question 9: The shrinking map in battle royale is called?",
+        //options include: Poison, Storm, Hurricane, Death
+        answer9: "Storm"
+    },
+    {
+        10: "Question 10: Which one of these is not a level of weapon?",
+        //options include: Rare, Epic, Normal, Legendary
+        answer: "Normal"
+    }
 ]
+
+// Create onClick command to start the game / timer / and question cycle.
+//$(document).ready(){
+    console.log(questions.question1);
+    console.log(questions.answer1);
+
+    var startGame = function () {
+        // Timer starts for 10 seconds
+        setTimeout(function () {
+            prompt(questions.question1);
+            
+        }, 10000);
+    };
+
+
+    // Display question 1 with choices displayed
+
+    // onClick event to capture user's choice
+
+    // Match with the answer in the array object to determine if answered correctly or not
+        // If correct, push to correctQs array
+        // If wrong, push to wrongQs array
+        // if unanswered, push to unansweredQs
+
+    // Display answer screen for 3 seconds to reveal if user's choice was correct or not
+
+    // Repeat for the 10 questions (for loop)
+
+    // Display recap page:
+    // This will show how many Qs the user got correct, wrong, unanswered
+
+
+    $(".start").click(function (startGame) {
+        var hello = "hello!";
+        alert(hello);
+    });
+
+
+
+//}
+
+
 
 //Pseudo Code:
 //1. game starts onClick of the 'start' button
